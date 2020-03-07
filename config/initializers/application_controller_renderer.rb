fd1ad24413cce74badb090b700d3b49b4b9aa673
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-03-07 14:02:55 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '45649eb422bba1b16169e5518648fdf5d1ce3ce9d12f74169e698e28d10f5e011a399a951a0b5c2d478734da849237ecc24a8edf8e4cfbe3836f233c0d6b7b66'
